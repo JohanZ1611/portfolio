9f73c2df38c2c63182fabd79ec3582ebbf446da4
Johan Zuluaga <johan16zulu@gmail.com>
2023-05-09 08:56:15 -0500

    contact terminado

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -2,49 +2,70 @@ import "./contact.css";
 
 export default function Contact() {
   return (
-    <div className=" h-screen bg-color-base flex flex-col gap-10 pt-20 justify-center items-center">
+    <div className=" h-screen bg-color-base flex flex-col gap-8 pt-10 justify-center items-center">
       <h2 className=" text-6xl font-bold text-center">
         Contactate <span className=" text-btn2-color">Conmigo!</span>
       </h2>
-      <form className="">
-        <div>
-          <div className="">
-            <input type="text" placeholder="full name" required />
-            <span className=""></span>
+      <form className=" max-w-3xl text-center z-0">
+        <div className=" relative flex justify-between flex-wrap">
+          <div className=" relative w-input my-3 mx-0">
+            <input
+              className=" w-full h-full p-6 text-base text-white bg-transparent  rounded-lg  borde"
+              type="text"
+              placeholder="Nombre Completo"
+              required
+            />
+            <span className="focus"></span>
           </div>
-          <div>
-            <input type="text" placeholder="Email Address" required />
-            <span className=""></span>
+          <div className=" relative w-input my-3 mx-0">
+            <input
+              type="text"
+              placeholder="Correo Electrónico"
+              className=" w-full h-full p-6 text-base text-white bg-transparent  rounded-lg  borde"
+              required
+            />
+            <span className=" focus"></span>
           </div>
         </div>
-        <div>
-          <div className="">
-            <input type="number" placeholder="Mobile Number" required />
-            <span className=""></span>
+        <div className="relative flex justify-between flex-wrap">
+          <div className=" relative w-input my-3 mx-0">
+            <input
+              className=" w-full h-full p-6 text-base text-white bg-transparent  rounded-lg borde"
+              type="number"
+              placeholder="Número de Teléfono"
+              required
+            />
+            <span className=" focus"></span>
           </div>
-          <div>
-            <input type="text" placeholder="Email Subject" required />
-            <span className=""></span>
+          <div className=" relative w-input my-3 mx-0">
+            <input
+              className=" w-full h-full p-6 text-base text-white bg-transparent rounded-lg borde"
+              type="text"
+              placeholder="Asunto del Correo"
+              required
+            />
+            <span className=" focus"></span>
           </div>
         </div>
 
-        <div>
+        <div className=" relative flex my-3 mx-0">
           <textarea
+            className=" resize-none w-full h-text p-6 text-base text-white bg-transparent rounded-lg borde"
             name=""
             id=""
             cols="30"
             rows="10"
-            placeholder="Your Message"
+            placeholder="tu Mensaje"
             required
           ></textarea>
-          <span className=""></span>
+          <span className="focus"></span>
         </div>
-        <div>
+        <div className=" w-full h-btn-home flex justify-center items-cente">
           <button
             type="submit"
-            className="btn cursor-pointer relative inline-flex justify-center items-center w-wid-btn2 h h-hei-btn bg-btn2-color  font-semibold text-xl tracking-widest text-color-base"
+            className="btn cursor-pointer relative inline-flex justify-center items-center w-wid-btn h-hei-btn bg-btn2-color  font-semibold text-xl tracking-widest text-color-base"
           >
-            Submit
+            Enviar
           </button>
         </div>
       </form>
