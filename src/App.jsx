17ce659214b01819c8489524accb7c288830a0a6
Johan Zuluaga <121687526+JohanZ1611@users.noreply.github.com>
2023-05-23 11:29:53 -0500

    Dev (#7)
    * libreria tailwind
    
    * nav
    
    * cont-home
    
    * home
    
    * about
    
    * about cambio imagen
    
    * skills
    
    * proyects
    
    * contact
    
    * contact terminado
    
    * union componentes
    
    * rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,6 @@
-import NavBar from "./components/NavBar/NavBar";
-import Home from "./pages/Home/Home";
-import About from "./pages/About/About";
-import Skills from "./pages/Skills/Skills";
-import Proyects from "./pages/Proyects/Proyects";
-import Contact from "./pages/Contact/Contact";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./pages";
 
 export default function App() {
-  return (
-    <div>
-      <NavBar />
-      <Home />
-      <About />
-      <Skills />
-      <Proyects />
-      <Contact />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
